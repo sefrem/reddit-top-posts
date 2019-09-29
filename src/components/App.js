5f2bb500125a6a578e4747f825c666ee0055cd6f
sefrem <47655937+sefrem@users.noreply.github.com>
2019-09-30 01:44:38 +0300

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,100 @@
+import React from "react";
+import Item from "./Item";
+import "../stylesheets/index.css";
+
+export class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      items: [],
+      isLoading: false,
+      enableAutoRefresh: false,
+      minComments: 0
+    };
+  }
+
+  componentDidMount() {
+    this.getItems();
+  }
+
+  getItems = () => {
+    this.setState({
+      isLoading: true
+    });
+    fetch("https://www.reddit.com/r/reactjs.json?limit=100")
+      .then(response => response.json())
+      .then(({ data }) => {
+        this.setState({
+          items: data.children,
+          isLoading: false
+        });
+      });
+  };
+
+  autoRefresh = () => {
+    this.setState(
+      state => ({
+        enableAutoRefresh: !state.enableAutoRefresh
+      }),
+      () => {
+        this.state.enableAutoRefresh
+          ? (this.refresh = setInterval(this.getItems, 3000))
+          : clearInterval(this.refresh);
+      }
+    );
+  };
+
+  updateMinComments = e => {
+    this.setState({
+      minComments: +e.target.value
+    });
+  };
+
+  getItemsByComments = (items, minComments) =>
+    items
+      .filter(item => item.data.num_comments >= minComments)
+      .sort((a, b) => b.data.num_comments - a.data.num_comments);
+
+  render() {
+    const { items, isLoading, enableAutoRefresh, minComments } = this.state;
+    const itemsSortByComments = this.getItemsByComments(items, minComments);
+
+    return (
+      <div>
+        <h1>Top Commented</h1>
+        <div>
+          <p>Current filter {minComments} </p>
+          <button
+            type="button"
+            style={{ marginBottom: "15px" }}
+            onClick={this.autoRefresh}
+          >
+            {enableAutoRefresh ? "Stop" : "Start"} auto-refresh
+          </button>
+        </div>
+
+        <input
+          className="inputRange"
+          type="range"
+          value={minComments}
+          onChange={this.updateMinComments}
+          min={0}
+          max={500}
+        />
+
+        <div className="content">
+          {isLoading ? (
+            <p>...Loading</p>
+          ) : itemsSortByComments.length > 0 ? (
+            itemsSortByComments.map(item => (
+              <Item key={item.data.id} data={item.data} />
+            ))
+          ) : (
+            <p>No results found matching your criteria</p>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/Item.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default class Item extends React.PureComponent {
+  render() {
+    console.log("render");
+    const { data } = this.props;
+    return (
+      <div className="item">
+        {data.thumbnail !== "self" && <img src={data.thumbnail} alt="" />}
+        <h4>{data.title}</h4>
+        <p>Number of comments: {data.num_comments}</p>
+        <a
+          href={`https://reddit.com/${data.permalink}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          link
+        </a>
+      </div>
+    );
+  }
+}
